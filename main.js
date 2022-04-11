3b34c166014416ecc210cd9c8c50deb4435dd208
Travis Ambrose <tmambrose@live.com>
2022-04-11 05:20:14 -0700

    Refactor code

--- main.js
+++ main.js
@@ -22,11 +22,12 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
-// Luhn algorithm: luhn()
-// Checks if digits are mathematically correct.
-const luhn = card => {
-    // Return false if not a valid card
-    if (!Array.isArray(card) || card.length < 15) return false;
+// validateCred(): Validate Credit Card
+// Return true when an array contains digits of a valid credit 
+// card number and false when it is invalid.
+const validateCred = card => {
+    // Return false if not an array.
+    if (!Array.isArray(card)) return false;
 
     let sum = 0;
     let isDbl = false;
@@ -45,13 +46,9 @@ const luhn = card => {
         }
     }
     
-    console.log(`Sum: ${sum}, Checker: ${sum%10}`);
+    //console.log(`Sum: ${sum}, Checker: ${sum%10}`);
 }
-luhn(valid3);
 
-// Validate Credit Card: validateCred()
-// If card has valid digits, then return true,
-// else false.
 
 
 
